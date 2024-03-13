263d518aff389c852c01c82bd7e98e3bbe8b6649
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-03-13 14:17:37 +0100

    Rel 2 Hecha

--- php-relacion2-emiliofernandez/comprobar.php
+++ php-relacion2-emiliofernandez/comprobar.php
@@ -8,11 +8,11 @@
 <p>Estos son los datos que nos has enviado:</p>
 <?php  
 echo "Nombre: "; echo $_POST['nombre']; echo "<br/>";
-echo "apellidos: "; echo $_POST['apellidos']; echo "<br/>";
+echo "Apellidos: "; echo $_POST['apellidos']; echo "<br/>";
 echo "E-mail: "; echo $_POST['email']; echo "<br/>";
-echo "contraseña: "; echo $_POST['contras']; echo "<br/>";
+echo "Contraseña: "; echo $_POST['contras']; echo "<br/>";
 echo "Sexo: "; echo $_POST['sexo']; echo "<br/>";
-echo "Estudios: "; echo $_POST['estudios']; echo "<br/>";
+echo "Físico: "; echo $_POST['fisico']; echo "<br/>";
 echo "Aficiones: \"on\" seleccionado, sin marcar, no seleccionado<br/>";
 if ((isset($_POST['musica']))){
 echo "Musica: "; echo $_POST['musica']; echo "<br/>";
@@ -29,7 +29,7 @@ if ((isset($_POST['libros']))){
 if ((isset($_POST['ciencia']))){
     echo "Ciencia: "; echo $_POST['ciencia']; echo "<br/><br/>";
 }
-echo "Día de la semana: "; echo $_POST['dia']; echo "<br/>";
+echo "Categoria Favorita: "; echo $_POST['categoria']; echo "<br/>";
 echo "Tu comentario: "; echo $_POST['comentario']; echo "<br/>";
 ?>
 <p>Comprueba tus datos antes de enviarlos, si no están bien vuelve a escribirlos.</p>
