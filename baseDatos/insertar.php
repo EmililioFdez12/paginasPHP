bdc08f1422eafdec0014545d57b70eb67523de9d
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-03-18 14:37:31 +0100

    No funciona

--- baseDatos/insertar.php
+++ baseDatos/insertar.php
@@ -1,29 +1,34 @@
 <?php
-
-$servidor = "localhost:3307"; // servidor local establecida en el puerto 3307
-                              // Hay que revisar el puerto que utiliza mySQLi
+$servidor = "localhost:3307";
 $usuario = "root";
 $pass = "";
 $bd = "prueba";
 
-// Establecemos conexión con la BD:
 $conexion = mysqli_connect($servidor, $usuario, $pass, $bd);
 
 if (!$conexion){
-      echo "No se puede conectar con el servidor";
-}else{  
-      $nombre = $_POST['nombre'];
-      $edad = $_POST['edad'];
+    echo "No se puede conectar con el servidor";
+} else {  
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $email = $_POST['email'];
+    $contraseña = $_POST['contras'];
+    $fisico = $_POST['fisico'];
+    $aficiones = isset($_POST['aficiones']) ? implode(",", $_POST['aficiones']) : "NS/NC";
+    $sexo = $_POST['sexo'];
+    $dia = $_POST['dia'];
+    $comentario = $_POST['comentario'];
 
-      //Introducimos los valores del formulario en la BD:
-      $sql = "INSERT INTO datos (nombre, edad) VALUES ('$nombre', '$edad')";
+    //Introducimos los valores del formulario en la BD:
+    $sql = "INSERT INTO datospersonales (Nombre, Apellidos, Email, Contraseña, Fisico, Aficiones, Sexo, Categoria, Opinion) 
+    VALUES ('$nombre', '$apellidos', '$email', '$contraseña', '$fisico', '$aficiones', '$sexo', '$dia', '$comentario')";
 
-      if (mysqli_query($conexion, $sql)) {
-            echo "Datos introducidos correctamente <br /><a href='./index.html'>Volver</a>";
-      } else {
-            echo "Error, no se ha podido establecer la conexión ";
-      }
-      // Cerramos la conexión:
-      mysqli_close($conexion);
+    if (mysqli_query($conexion, $sql)) {
+        echo "Datos introducidos correctamente <br /><a href='./index.html'>Volver</a>";
+    } else {
+        echo "Error, no se ha podido establecer la conexión ";
+    }
+
+    mysqli_close($conexion);
 } 
 ?>
